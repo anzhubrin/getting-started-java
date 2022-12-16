70a8af5232badbe95ee282eceddba60945fa75ff
anzhubrin <1111>
2022-12-16 09:13:22 +0300

    edit main class

--- src/test/java/GoogleTests.java
+++ src/test/java/GoogleTests.java
@@ -7,6 +7,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class GoogleTests {
     @Test
     void successfulSearchTest() {
+
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
