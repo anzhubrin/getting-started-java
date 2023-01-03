e1bacbf42a23f46df44c61b50a80ba6d935ba5eb
anzhubrin <1111>
2023-01-03 11:51:48 +0300

    refactoring RegistrationForm

--- src/test/java/demoqa/RegistrationForm.java
+++ src/test/java/demoqa/RegistrationForm.java
@@ -11,28 +11,29 @@ import static com.codeborne.selenide.Selenide.*;
 public class RegistrationForm {
     @Test
     void studentRegistrationForm() {
+        // Открытие сайта
         open("https://demoqa.com/automation-practice-form");
-        $("#firstName").setValue("Andrew");
-        $("#lastName").setValue("Smith");
-        $("#userEmail").setValue("and1smith@mail.com");
+        $("#firstName").setValue("Andrew"); // Вводим имя
+        $("#lastName").setValue("Smith"); // Вводим фамилию
+        $("#userEmail").setValue("and1smith@mail.com"); // Вводим email
 
-        $(".custom-radio:nth-child(1) > .custom-control-label").click();
-        $("#userNumber").setValue("9324567700");
+        $(".custom-radio:nth-child(1) > .custom-control-label").click(); // Выбираем пол
+        $("#userNumber").setValue("9324567700"); // Вводим номер телефона
 
-        $("#subjectsInput").val("Olaolala");
+        $("#subjectsInput").val("Olaolala"); // Вводим subjects
 
-        $(byText("Sports")).click();
+        $(byText("Sports")).click(); // Выбираем хобби
 
-        File file = new File("src/test/resources/readme.txt");
+        File file = new File("src/test/resources/readme.txt"); // Загружаем файл
         $("#uploadPicture").uploadFromClasspath("readme.txt");
 
-        $("#currentAddress").setValue("Guzelce 33");
-        $(byText("Select State")).click();
+        $("#currentAddress").setValue("Guzelce 33"); // Вводим адресс
+        $(byText("Select State")).click(); // Выбираем штат
         $(byText("NCR")).click();
-        $(byText("Select City")).click();
+        $(byText("Select City")).click(); // Выбираем город
         $(byText("Delhi")).click();
 
-        $("#submit").click();
+        $("#submit").click(); // Подтверждаем
 
         $("#example-modal-sizes-title-lg").shouldHave((text("Thanks for submitting the form")));
     }
